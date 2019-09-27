7d2a5203b9de8bdaacc80bdc9a04c4a06ef102bf
flowdx <flowdx@users.noreply.github.com>
2019-09-27 15:10:22 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+mkdir -p ~/crm114       # Erzeuge Ordner 'crm114'
+cd ~/crm114             # Wechsle in den Ordner 'crm114'
+curl -sSL http://crm114.sourceforge.net/tarballs/crm114-20100106-BlameMichelson.src.tar.gz | tar xz # Kopiere und entpacke $ CRM114
+cd crm114-20*           # Wechsle in den CRM114-Programmordner
+curl -sSL https://laurikari.net/tre/tre-0.8.0.tar.bz2 | tar xj
+cd tre*
+./configure --prefix "`cd ..; pwd`/tre" --enable-static
+make install
+cd ..
+sed -i 's/^LDFLAGS/#LDFLAGS/' Makefile
+sed -i 's|^\([^#].*\)-ltre|\1tre/lib/libtre.a|' Makefile
+CFLAGS="-std=gnu89 -Wno-unused-but-set-variable -I tre/include" LDFLAGS="$CFLAGS" make
+strip -s crm114 cssdiff cssmerge cssutil osbf-util
+cp -p crm114 cssdiff cssmerge cssutil osbf-util ..
+cp -p mailfilter.crm maillib.crm mailreaver.crm mailtrainer.crm rewriteutil.crm shuffle.crm ..
+chmod 755 ../*.crm
+for i in mailfilter.cf blacklist.mfp priolist.mfp rewrites.mfp; do cp -p $i ../$i.example; done
+cp -p whitelist.mfp.example ..
+cd ..
+rm -r crm114-20*
+curl -sSL https://github.com/flowdx/crm114_u7/archive/master.zip -o master.zip # Die Konfigurationsdateien aus diesem Github-Repository herunterladen
+unzip master.zip # Das gezippte Repository entpacken
+rm master.zip    # Das gezippte Repository löschen
+mv ./crm114_u7-master/* ./ # Die Dateien aus dem Entpackordner in den Hauptordner verschieben
+rm -r crm114_u7-master     # Den Entpackordner löschen
+chmod 755 cache_cleanup crm114 cssdiff cssmerge cssutil db_init learn_maildir # Die korrekte Berechtigung für die ausführbaren Dateien vergeben
+sh db_init # Die Einrichtung der Datenbank durchführen
+cd ~ # Zurück in den $USER-Ordner
